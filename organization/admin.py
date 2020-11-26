8c39f87a8e8462def3bec0f09b0a949034c07d6f
unknown <sirdesmond09@gmail.com>
2020-11-26 10:18:51 +0100

    admin site changes

--- organization/admin.py
+++ organization/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from .models import EcoSystem, Organization
 
 # Register your models here.
-admin.site.register(Organization)
 admin.site.register(EcoSystem)
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_editable = ['is_entrepreneur', 'is_ecosystem', 'is_active']
