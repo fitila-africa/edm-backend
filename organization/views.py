e17f76e907453921398a290dd96bffc62aa6f3ff
sirdesmond09 <sirdesmond09@gmail.com>
2021-08-16 16:08:33 +0000

    debug changes

--- organization/views.py
+++ organization/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view, authentication_classes, permission_classes 
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import exceptions, status
@@ -18,10 +18,10 @@ from .populate import process_data
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def organizations(request):
-    
+
     if request.method == 'GET':
         organization = Organization.objects.all().filter(is_active=True).filter(is_approved=True)
-    
+
         serializer = OrganizationSerializer(organization, many =True)
 
         data = {
@@ -39,18 +39,18 @@ def organizations(request):
 def add_organization(request):
 
     if request.method == 'POST':
-        
+
         serializer = OrganizationSerializer(data = request.data)
 
         if serializer.is_valid():
             try:
                 if serializer.validated_data['ceo_image']:
                     # upload the ceo's image
-                    ceo_image = serializer.validated_data['ceo_image'] #get the image file from the request 
+                    ceo_image = serializer.validated_data['ceo_image'] #get the image file from the request
                     img2 = cloudinary.uploader.upload(ceo_image, folder = 'fitila/ceo_image/') #upload the image to cloudinary
                     serializer.validated_data['ceo_image'] = "" #delete the image file
-                    serializer.validated_data['ceo_image_url'] = img2['secure_url'] #save the image url 
-                else: 
+                    serializer.validated_data['ceo_image_url'] = img2['secure_url'] #save the image url
+                else:
                     data = {
                     'status'  : False,
                     'message' : "Unsuccessful",
@@ -58,16 +58,16 @@ def add_organization(request):
                     }
 
                     return Response(data, status = status.HTTP_400_BAD_REQUEST)
-                
+
                 if serializer.validated_data['company_logo']:
                 #upload the company's logo
-                    company_logo = serializer.validated_data['company_logo'] #get the image file from the request 
+                    company_logo = serializer.validated_data['company_logo'] #get the image file from the request
                     img1 = cloudinary.uploader.upload(company_logo, folder = 'fitila/company_logo/') #upload the image to cloudinary
                     serializer.validated_data['company_logo'] = "" #delete the image file
-                    serializer.validated_data['company_logo_url'] = img1['secure_url'] #save the image url 
-                
+                    serializer.validated_data['company_logo_url'] = img1['secure_url'] #save the image url
 
-                
+
+
             except Exception:
                 print(Exception)
                 data = {
@@ -76,7 +76,7 @@ def add_organization(request):
                     'error' : ["Unable to add organization"],
                 }
                 return Response(data, status = status.HTTP_400_BAD_REQUEST)
-            
+
             organization = Organization.objects.create(**serializer.validated_data)
 
             serializer = OrganizationSerializer(organization)
@@ -99,13 +99,13 @@ def add_organization(request):
 
 
 @swagger_auto_schema(methods=['PUT', 'DELETE'], request_body=OrganizationSerializer())
-@api_view(['GET', 'PUT', 'DELETE']) 
+@api_view(['GET', 'PUT', 'DELETE'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def organization_detail(request, pk):
     try:
         organization =  Organization.objects.get(pk = pk, is_active =True)
-    
+
     except Organization.DoesNotExist:
         data = {
                 'status'  : False,
@@ -116,7 +116,7 @@ def organization_detail(request, pk):
 
     if request.method == 'GET':
         serializer = OrganizationSerializer(organization)
-        
+
         data = {
                 'status'  : True,
                 'message' : "Successful",
@@ -133,16 +133,16 @@ def organization_detail(request, pk):
             try:
 
                 if 'ceo_image' in serializer.validated_data.keys():
-                    ceo_image = serializer.validated_data['ceo_image'] #get the image file from the request 
+                    ceo_image = serializer.validated_data['ceo_image'] #get the image file from the request
                     img2 = cloudinary.uploader.upload(ceo_image, folder = 'fitila/ceo_image/') #upload the image to cloudinary
                     serializer.validated_data['ceo_image'] = "" #delete the image file
                     serializer.validated_data['ceo_image_url'] = img2['secure_url'] #save the image url
 
                 if 'company_logo' in serializer.validated_data.keys():
-                    company_logo = serializer.validated_data['company_logo'] #get the image file from the request 
+                    company_logo = serializer.validated_data['company_logo'] #get the image file from the request
                     img1 = cloudinary.uploader.upload(company_logo, folder = 'fitila/company_logo/') #upload the image to cloudinary
                     serializer.validated_data['company_logo'] = "" #delete the image file
-                    serializer.validated_data['company_logo_url'] = img1['secure_url'] #save the image url 
+                    serializer.validated_data['company_logo_url'] = img1['secure_url'] #save the image url
             except Exception:
                 data = {
                     'status'  : False,
@@ -173,9 +173,9 @@ def organization_detail(request, pk):
 
     #de-activate the item
     elif request.method == 'DELETE':
-        
+
         organization.delete()
-        
+
         data = {
                 'status'  : True,
                 'message' : "Deleted Successfully"
@@ -189,7 +189,7 @@ def organization_detail(request, pk):
 def pending_organizations(request):
     if request.method == 'GET':
         organization = Organization.objects.all().filter(is_active=True).filter(responded =False)
-    
+
         serializer = OrganizationSerializer(organization, many =True)
 
         data = {
@@ -206,10 +206,10 @@ def pending_organizations(request):
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def ecosystem(request):
-    
+
     if request.method == 'GET':
         eco = EcoSystem.objects.filter(is_active=True)
-    
+
         serializer = EcosystemSerializer(eco, many =True)
 
         data = {
@@ -222,11 +222,11 @@ def ecosystem(request):
 
 
     elif request.method == 'POST':
-        
+
         serializer = EcosystemSerializer(data = request.data)
-        
+
         if serializer.is_valid():
-        
+
             eco = EcoSystem.objects.create(**serializer.validated_data)
             eco.save()
 
@@ -250,13 +250,13 @@ def ecosystem(request):
 
 
 @swagger_auto_schema(methods=['PUT', 'DELETE'], request_body=EcosystemSerializer())
-@api_view(['GET', 'PUT', 'DELETE']) 
+@api_view(['GET', 'PUT', 'DELETE'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def ecosystem_detail(request, pk):
     try:
         eco =  EcoSystem.objects.get(pk = pk, is_active=True)
-    
+
     except EcoSystem.DoesNotExist:
         data = {
                 'status'  : False,
@@ -267,7 +267,7 @@ def ecosystem_detail(request, pk):
 
     if request.method == 'GET':
         serializer = EcosystemSerializer(eco)
-        
+
         data = {
                 'status'  : True,
                 'message' : "Successful",
@@ -304,15 +304,15 @@ def ecosystem_detail(request, pk):
 
     #de-activate the item
     elif request.method == 'DELETE':
-        
+
         eco.delete()
-        
+
         data = {
                 'status'  : True,
                 'message' : "Deleted Successfully"
             }
 
-        return Response(data, status = status.HTTP_204_NO_CONTENT)       
+        return Response(data, status = status.HTTP_204_NO_CONTENT)
 
 
 @swagger_auto_schema(methods=['POST'], request_body=SubecosystemSerializer())
@@ -320,10 +320,10 @@ def ecosystem_detail(request, pk):
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def sub_ecosystem(request):
-    
+
     if request.method == 'GET':
         eco = SubEcosystem.objects.filter(is_active=True)
-    
+
         serializer = SubecosystemSerializer(eco, many =True)
 
         data = {
@@ -336,14 +336,14 @@ def sub_ecosystem(request):
 
 
     elif request.method == 'POST':
-        
+
         serializer = SubecosystemSerializer(data = request.data)
-        
+
         if serializer.is_valid():
-        
+
             serializer.save()
 
-            
+
             data = {
                 'status'  : True,
                 'message' : "Successful",
@@ -363,13 +363,13 @@ def sub_ecosystem(request):
 
 
 @swagger_auto_schema(methods=['PUT'], request_body=SubecosystemSerializer())
-@api_view(['GET', 'PUT', 'DELETE']) 
+@api_view(['GET', 'PUT', 'DELETE'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def sub_ecosystem_detail(request, pk):
     try:
         eco =  SubEcosystem.objects.get(pk = pk, is_active=True)
-    
+
     except SubEcosystem.DoesNotExist:
         data = {
                 'status'  : False,
@@ -380,7 +380,7 @@ def sub_ecosystem_detail(request, pk):
 
     if request.method == 'GET':
         serializer = SubecosystemSerializer(eco)
-        
+
         data = {
                 'status'  : True,
                 'message' : "Successful",
@@ -417,25 +417,25 @@ def sub_ecosystem_detail(request, pk):
 
     #de-activate the item
     elif request.method == 'DELETE':
-        
+
         eco.delete()
-        
+
         data = {
                 'status'  : True,
                 'message' : "Deleted Successfully"
             }
 
-        return Response(data, status = status.HTTP_204_NO_CONTENT) 
+        return Response(data, status = status.HTTP_204_NO_CONTENT)
 
 @swagger_auto_schema(methods=['POST'], request_body=SectorSerializer())
 @api_view(['GET', 'POST'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def sector(request):
-    
+
     if request.method == 'GET':
         obj = Sector.objects.filter(is_active=True)
-    
+
         serializer = SectorSerializer(obj, many =True)
 
         data = {
@@ -448,11 +448,11 @@ def sector(request):
 
 
     elif request.method == 'POST':
-        
+
         serializer = SectorSerializer(data = request.data)
-        
+
         if serializer.is_valid():
-        
+
             obj = Sector.objects.create(**serializer.validated_data)
             obj.save()
 
@@ -476,13 +476,13 @@ def sector(request):
 
 
 @swagger_auto_schema(methods=['PUT'], request_body=SectorSerializer())
-@api_view(['GET', 'PUT', 'DELETE']) 
+@api_view(['GET', 'PUT', 'DELETE'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def sector_detail(request, pk):
     try:
         obj =  Sector.objects.get(pk = pk, is_active=True)
-    
+
     except Sector.DoesNotExist:
         data = {
                 'status'  : False,
@@ -493,7 +493,7 @@ def sector_detail(request, pk):
 
     if request.method == 'GET':
         serializer = SectorSerializer(obj)
-        
+
         data = {
                 'status'  : True,
                 'message' : "Successful",
@@ -530,26 +530,26 @@ def sector_detail(request, pk):
 
     #de-activate the item
     elif request.method == 'DELETE':
-        
+
         obj.delete()
-        
+
         data = {
                 'status'  : True,
                 'message' : "Deleted Successfully"
             }
 
-        return Response(data, status = status.HTTP_204_NO_CONTENT)  
-    
-    
+        return Response(data, status = status.HTTP_204_NO_CONTENT)
+
+
 @swagger_auto_schema(methods=['POST'], request_body=SubecosystemSubclassSerializer())
 @api_view(['GET', 'POST'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def subclass(request):
-    
+
     if request.method == 'GET':
         obj = SubecosystemSubclass.objects.filter(is_active=True)
-    
+
         serializer = SubecosystemSubclassSerializer(obj, many =True)
 
         data = {
@@ -562,11 +562,11 @@ def subclass(request):
 
 
     elif request.method == 'POST':
-        
+
         serializer = SubecosystemSubclassSerializer(data = request.data)
-        
+
         if serializer.is_valid():
-        
+
             serializer.save()
             data = {
                 'status'  : True,
@@ -587,13 +587,13 @@ def subclass(request):
 
 
 @swagger_auto_schema(methods=['PUT'], request_body=SubecosystemSubclassSerializer())
-@api_view(['GET', 'PUT', 'DELETE']) 
+@api_view(['GET', 'PUT', 'DELETE'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
 def subclass_detail(request, pk):
     try:
         obj =  SubecosystemSubclass.objects.get(pk = pk, is_active=True)
-    
+
     except SubecosystemSubclass.DoesNotExist:
         data = {
                 'status'  : False,
@@ -604,7 +604,7 @@ def subclass_detail(request, pk):
 
     if request.method == 'GET':
         serializer = SubecosystemSubclassSerializer(obj)
-        
+
         data = {
                 'status'  : True,
                 'message' : "Successful",
@@ -641,15 +641,15 @@ def subclass_detail(request, pk):
 
     #de-activate the item
     elif request.method == 'DELETE':
-        
+
         obj.delete()
-        
+
         data = {
                 'status'  : True,
                 'message' : "Deleted Successfully"
             }
 
-        return Response(data, status = status.HTTP_204_NO_CONTENT)  
+        return Response(data, status = status.HTTP_204_NO_CONTENT)
 
 
 
@@ -660,8 +660,8 @@ def state(request):
 
 
     filtered = {state[i]['state']: list(map(lambda x: {"id":x.id,
-                                                "name":x.name, 
-                                                "company_logo_url": x.company_logo_url, "ceo_image_url":x.ceo_image_url, "state":x.state, 
+                                                "name":x.name,
+                                                "company_logo_url": x.company_logo_url, "ceo_image_url":x.ceo_image_url, "state":x.state,
                                                 "sector":x.sector.name,
                                                 "employee" :x.num_of_employees, "funding":x.funding
                                                 },Organization.objects.filter(state = state[i]['state']))) for i in range(len(state))}
@@ -678,14 +678,14 @@ def state(request):
 @api_view(['POST'])
 def upload_csv(request):
 
-    
+
     if request.method == 'POST':
-        
+
         serializer = FileUploadSerializer(data = request.data)
-        
+
 
         if serializer.is_valid():
-            
+
             file = serializer.validated_data['file']
             try:
 
@@ -695,14 +695,14 @@ def upload_csv(request):
                 'status'  : False,
                 'message' : "Unsuccessful",
                 'errors'  : ["Error uploading file.\n Please check that headers are correct."]
-                
+
                 }
                 return Response(data, status = status.HTTP_400_BAD_REQUEST)
-            
+
             # decoded_file = file.read().decode()
-            
+
             # io_string = io.StringIO(decoded_file)
-            
+
             # reader = csv.DictReader(io_string)
             success = False
             for row in rows:
@@ -711,7 +711,7 @@ def upload_csv(request):
                     row['sector'] = Sector.objects.get(name = str(row['sector']))
                     row['ecosystem'] = EcoSystem.objects.get(name = str(row['ecosystem']))
                     row['sub_ecosystem'] = SubEcosystem.objects.get(name = str(row['sub_ecosystem']), ecosystem=row['ecosystem'])
-                    
+
                     Organization.objects.create(**row, is_active=True, is_approved=True, responded=True)
 
                     success = True
@@ -721,7 +721,7 @@ def upload_csv(request):
                 data = {
                     'status'  : True,
                     'message' : "File upload successful",
-                    
+
                 }
 
                 return Response(data, status = status.HTTP_200_OK)
@@ -730,7 +730,7 @@ def upload_csv(request):
                 'status'  : False,
                 'message' : "Unsuccessful",
                 'errors'  : ["Error uploading file.\n Please check that fields are correct."]
-                
+
             }
 
             return Response(data, status = status.HTTP_400_BAD_REQUEST)
@@ -740,7 +740,7 @@ def upload_csv(request):
                 'status'  : False,
                 'message' : "Unsuccessful",
                 'errors'  : serializer.errors
-                
+
             }
 
             return Response(data, status = status.HTTP_400_BAD_REQUEST)
@@ -761,7 +761,7 @@ def approve_org(request, org_id):
         return Response(data, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        
+
         object.is_approved = True
         object.responded = True
         object.save()
@@ -789,7 +789,7 @@ def approve_org(request, org_id):
 
 @api_view(['GET'])
 def reject_org(request, org_id):
-    
+
     try:
         object = Organization.objects.get(id=org_id)
 
@@ -802,7 +802,7 @@ def reject_org(request, org_id):
         return Response(data, status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        
+
         object.is_declined = True
         object.responded = True
         object.save()
@@ -832,7 +832,7 @@ def reject_org(request, org_id):
 def rejected_org(request):
     if request.method == 'GET':
         organization = Organization.objects.all().filter(is_active=True).filter(is_declined =True)
-    
+
         serializer = OrganizationSerializer(organization, many =True)
 
         data = {
