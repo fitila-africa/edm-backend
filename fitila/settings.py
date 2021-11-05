951eb5828750c6ae4c97ab2f6c68900a42450f22
sirdesmond09 <sirdesmond09@gmail.com>
2021-11-05 14:38:18 +0100

    chage email port

--- fitila/settings.py
+++ fitila/settings.py
@@ -264,9 +264,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587 
-# EMAIL_USE_SSL = True    # use port 465
-EMAIL_USE_TLS = True    # use port 587
+EMAIL_PORT = 465 
+EMAIL_USE_SSL = True    # use port 465
+# EMAIL_USE_TLS = True    # use port 587
 
 DEFAULT_FROM_EMAIL = 'EDM PLATFORM <noreply@ecomap.ng>'
 
