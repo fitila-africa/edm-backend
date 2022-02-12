27c79b8ef794003b198d450aba6d437324aecb48
sirdesmond09 <sirdesmond09@gmail.com>
2022-02-12 13:42:14 +0100

    db settings

--- fitila/settings.py
+++ fitila/settings.py
@@ -39,17 +39,26 @@ if env == 'staging':
     DEBUG = bool(0)
     
     ALLOWED_HOSTS = ['*']
+    # DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST'),
+    #     'PORT': "",
+    #     }
+    # }
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': "",
+        'PORT': 5432,
         }
     }
-    
 elif env == 'production':
         
     DEBUG = bool(0)
