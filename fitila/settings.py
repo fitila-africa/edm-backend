a6fc50c2945d7f3594ce34f3aa20e30057c49b02
sirdesmond09 <sirdesmond09@gmail.com>
2022-02-16 12:55:57 +0100

    remove static storage

--- fitila/settings.py
+++ fitila/settings.py
@@ -205,7 +205,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'build/static')
 # ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
